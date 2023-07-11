b3c828d48ff003f948a6677464752680a08228c6
Minhaj <minhajulislammuhammad@gmail.com>
2023-07-11 13:11:24 +0600

    added Inorder Traversal.cpp

---
+++ Inorder_Traversal.cpp
@@ -0,0 +1,21 @@
+// Q: https://www.codingninjas.com/studio/problems/inorder-traversal_3839605?leftPanelTab=0
+
+vector<int> v;
+void inorderTraverse(TreeNode *root)
+{
+    if (root == NULL)
+        return;
+    if (root->left)
+        inorderTraverse(root->left);
+    v.push_back(root->data);
+    if (root->right)
+        inorderTraverse(root->right);
+}
+vector<int> getInOrderTraversal(TreeNode *root)
+{
+    v.clear(); // in this problem tese cases are used. So, beacuse of we took a global
+               // vector we have to clear the vector before every test case. otherwise
+               //  previous values will remain in the vector.
+    inorderTraverse(root);
+    return v;
+}
